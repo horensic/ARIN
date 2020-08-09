7c5acd53050fd0352f0a0f1b9f90fae075e1aec9
horensic <horensic@gmail.com>
2020-08-09 19:30:19 +0900

    code cleanup

--- logfile/analyzer.py
+++ logfile/analyzer.py
@@ -1,17 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-#           OH MY GIRL License
-#   To create a program using this source code,
-#   Follow the link below to listen to the OH MY GIRL's song at least once.
-#   LINK (1): https://youtu.be/RrvdjyIL0fA
-#   LINK (2): https://youtu.be/QIN5_tJRiyY
-#   LINK (3): https://youtu.be/udGwca1HBM4
-#   LINK (4): https://youtu.be/QTD_yleCK9Y
 
 """
 @author:    Seonho Lee
-@license:   OH_MY_GIRL License
 @contact:   horensic@gmail.com
 """
 
